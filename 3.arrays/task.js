5b0635e2dfd31549845126a4c6ef760b13dda345
Farid Mukhametkhanov <fred6776@yandex.com>
2026-01-01 14:10:16 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((currentValue, index) => currentValue === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let filtered = users.filter(user => user.gender === gender);
+  if (filtered.length === 0) {
+    return 0;
+  }
+  let result = filtered.map(user => user.age).reduce((sum, age) => sum + age);
+  return result / filtered.length;
 }
